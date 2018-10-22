9f75db98fbb9d04d8a13fe954614cabbcd7c4785
Manuel <manuelantoniopadilla@gmail.com>
2018-10-21 23:39:23 -0500

    Add the starter files

---
+++ liri.js
@@ -0,0 +1,8 @@
+// Read and set environment variables
+require("dotenv").config();
+
+// prints `34e84d93de6a4650815e5420e0` to the console
+console.log(process.env.SPOTIFY_ID) 
+
+// prints `5162cd8b5cf940f48702df` to the console
+console.log(process.env.SPOTIFY_SECRET)
